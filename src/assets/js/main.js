25e929017fe00d0d8d583801f342964303482cfb
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-09-25 17:39:11 +0300

    04. usefull-things-192-draggable. fix.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,5 @@
-import * as THREE from "three";
+import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/0.180.0/three.module.js";
+
 import { projects } from "./data.js";
 import { vertexShader, fragmentShader } from "./shaders.js";
 
